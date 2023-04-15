8073e4338c619d1ca69802ecdfdd5f001f87156f
ci-cada <ciphedc@gmail.com>
2023-04-15 15:03:21 +0300

    Day 11 File Change

---
+++ Day_11/functions_3.py
@@ -0,0 +1,29 @@
+# 10. Declare a function named capitalize_list_items. It takes a list as a parameter and it returns a capitalized list of items
+# 11. Declare a function named add_item. It takes a list and an item parameters. It returns a list with the item added at the end.
+
+# ```py
+# food_staff = ['Potato', 'Tomato', 'Mango', 'Milk'];
+# print(add_item(food_staff, 'Meat'))     # ['Potato', 'Tomato', 'Mango', 'Milk','Meat'];
+# numbers = [2, 3, 7, 9];
+# print(add_item(numbers, 5))      [2, 3, 7, 9, 5]
+# ```
+
+# 12. Declare a function named remove_item. It takes a list and an item parameters. It returns a list with the item removed from it.
+
+# ```py
+# food_staff = ['Potato', 'Tomato', 'Mango', 'Milk'];
+# print(remove_item(food_staff, 'Mango'))  # ['Potato', 'Tomato', 'Milk'];
+# numbers = [2, 3, 7, 9];
+# print(remove_item(numbers, 3))  # [2, 7, 9]
+# ```
+
+# 13. Declare a function named sum_of_numbers. It takes a number parameter and it adds all the numbers in that range.
+
+# ```py
+# print(sum_of_numbers(5))  # 15
+# print(sum_all_numbers(10)) # 55
+# print(sum_all_numbers(100)) # 5050
+# ```
+
+# 14. Declare a function named sum_of_odds. It takes a number parameter and it adds all the odd numbers in that range.
+# 15. Declare a function named sum_of_even. It takes a number parameter and it adds all the even numbers in that - range.
---
+++ Day_11/functions_4.py
@@ -0,0 +1,11 @@
+# 1.  Declare a function named evens_and_odds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+
+# ```py
+#     print(evens_and_odds(100))
+#     # The number of odds are 50.
+#     # The number of evens are 51.
+# ```
+
+# 1. Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
+# 1. Call your function _is_empty_, it takes a parameter and it checks if it is empty or not
+# 1. Write different functions which take lists. They should calculate_mean, calculate_median, calculate_mode, calculate_range, calculate_variance, calculate_std (standard deviation).
